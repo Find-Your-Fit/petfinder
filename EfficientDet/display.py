33b7f9180945807922cde24d1f77b1dae6078c26
MG Park <tieckit@gmail.com>
2022-03-11 17:52:13 +0900

    Revert "#2 Using pretrained vgg16 obejct detection, get bounding boxes & classes"
    This reverts commit b5b88248ca8aafa1ab32dde1fa075c8bff2ab584.

---
+++ EfficientDet/display.py
@@ -0,0 +1,51 @@
+
+obj_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+            'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+            'cow', 'elephant', 'bear', 'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
+            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+            'skateboard', 'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
+            'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
+            'cake', 'chair', 'couch', 'potted plant', 'bed', '', 'dining table', '', '', 'toilet', '', 'tv',
+            'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+            'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+            'toothbrush']
+
+
+import cv2
+import tqdm
+import numpy as np
+
+def display(preds, imshow=True, imwrite=False): #imgs
+
+    compound_coef = 6
+    col_class, col_points = [], []
+    for i in range(len(preds)):
+        if len(preds[i]['rois']) == 0:
+            continue
+        
+
+        row_class,row_points = [], []
+        # imgs[i] = imgs[i].copy()
+
+        for j in range(len(preds[i]['rois'])):
+            x1, y1, x2, y2 = preds[i]['rois'][j].astype(np.int)
+            obj = obj_list[preds[i]['class_ids'][j]]
+            score = float(preds[i]['scores'][j])
+            # plot_one_box(imgs[i], [x1, y1, x2, y2], label=obj,score=score,color=color_list[get_index_label(obj, obj_list)])
+
+            if obj in ["cat","dog"]:
+                row_class.append(obj)
+                row_points.append((x1, y1, x2, y2))
+
+
+        if imshow:
+            pass
+            # cv2.imshow('img', imgs[i])
+            # cv2.waitKey(0)
+
+        if imwrite:
+            cv2.imwrite(f'test/img_inferred_d{compound_coef}_this_repo_{i}.jpg', imgs[i])
+    col_class.append(row_class)
+    col_points.append(row_points)
+
+    return col_points,col_class
